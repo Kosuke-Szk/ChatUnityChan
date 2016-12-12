6e2911567122c4722249d57c92cd391d6ef3d34a
鈴木航介 <hekk106@HEKK106s-MBP.local>
2016-12-12 19:20:52 +0900

    Create AR system

---
+++ ChatBot/Assets/Scripts/LogPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LogPanel : MonoBehaviour 
+{
+    private bool isOpened;
+
+    public Text _log;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+
+    public void OnClick()
+    {
+        if (!isOpened)
+        {
+            this.gameObject.SetActive(true);
+            isOpened = true;
+        } else
+        {
+            this.gameObject.SetActive(false);
+            isOpened = false;
+        }
+    }
+}
